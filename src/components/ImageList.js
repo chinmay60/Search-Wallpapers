e00506061b9fc94269a4e605af0341eb1734755b
Chinmay Vinchurkar <chinmayvinchurkar24@gmail.com>
2020-10-02 19:26:46 -0400

    aligned images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './ImageList.css'
 
 const ImageList = props => {
   const images = props.images.map(image => {
     return <img atl = {image.description} key={image.id} src={image.urls.regular} />;
   });
 
-  return <div>{images}</div>;
+  return <div className='image-list'>{images}</div>;
 };
 
 
